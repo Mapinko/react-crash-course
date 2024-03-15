c171789bb98bb825a3a9896c22683896bbb5386a
Mari Spinola <mapincosf@gmail.com>
2024-03-15 14:17:26 -0300

    feat: open and close modal logic

--- react-crash-course/src/components/PostsList/index.jsx
+++ react-crash-course/src/components/PostsList/index.jsx
@@ -5,9 +5,13 @@ import Post from '../Post';
 import classes from './PostsList.module.css';
 
 function PostsList() {
-
+   const [modalIsVisible, setModalIsVisible] = useState(true);
    const [enteredBody, setEnteredBody] = useState('');
    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+   function hideModalHandler() {
+      setModalIsVisible(false)
+   }
 
    function bodyChangeHandler(event) {
       setEnteredBody(event.target.value);
@@ -16,14 +20,23 @@ function PostsList() {
       setEnteredAuthor(event.target.value);
    }
 
-   return (
-      <>
-         <Modal>
-            <NewPost 
-               onBodyChange={bodyChangeHandler} 
+   let modalContent
+
+   if (modalIsVisible) {
+      modalContent = (
+         <Modal onClose={hideModalHandler}>
+            <NewPost
+               onBodyChange={bodyChangeHandler}
                onAuthorChange={authorChangeHandler}
             />
          </Modal>
+      )
+
+   }
+   return (
+      <>
+         {modalContent}
+
          <ul className={classes.posts}>
             <Post author={enteredAuthor} body={enteredBody}/>
          </ul>`
